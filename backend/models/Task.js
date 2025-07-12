50acae2c425cf2fcc281c440da0b4806ef9f5a0e
AnvetaDigital <nangarea2017@gmail.com>
2025-07-12 22:48:35 +0530

    [feat]: add Task mongoose model with schema

---
+++ backend/models/Task.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    dueDate: {
+        type: Date
+    },
+    priority: {
+        type: String,
+        enum: ['Low', 'Medium', 'High'],
+        default: 'Medium'
+    },
+    status: {
+        type: String,
+        enum: ['Incomplete', 'Complete'],
+        default: 'Incomplete'
+    }
+}, { timestamps: true });
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
