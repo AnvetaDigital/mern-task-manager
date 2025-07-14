0eb893f026fb3e3212d82fda941ef015b94f501f
Anveta Nangare <121052288+AnvetaDigital@users.noreply.github.com>
2025-07-14 19:26:33 +0530

    update authController.js
    remove provided path from dotenv's config function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs'
 import jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '../.env' });
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
