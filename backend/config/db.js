8781f79171cffece815b63e961c9a4d677d0f945
AnvetaDigital <nangarea2017@gmail.com>
2025-08-15 17:53:38 +0530

    [fix]: remove logged statement

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Atlass connected successfully!");
   } catch (error) {
     console.error(`error getting while mongoDB connection: ${error}`);
     process.exit(1);
