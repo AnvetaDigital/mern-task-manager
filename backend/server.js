c12a5a78a7dbcdfc2b2a5f11639c6308a3e3cca4
AnvetaDigital <nangarea2017@gmail.com>
2025-08-15 17:56:27 +0530

    [fix]: update route for server

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use("/api/auth", router);
 app.use("/api/tasks", taskRouters);
 
-app.get("/api/get", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello from express.js");
 });
 
